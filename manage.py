4a38977970ddaf137b1a5f941dd7b5a65109c816
lederp23 <munalaoliverngoitsi@gmail.com>
2017-04-03 11:46:07 +0300

    Modified manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from flask_cors import CORS, cross_origin
 from config import ProductionConfig
 from urls import urls
 import os
-from app import app, db
+from app import *
 from v1.api.models import BucketList, Item
 from v1.accounts.models import User
 
