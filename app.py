90b3ee1350f8a50c2c1a9f08080c5cb9348478ab
lederp23 <munalaoliverngoitsi@gmail.com>
2017-04-03 10:15:05 +0300

    Modified manage.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import ProductionConfig
 
 app = Flask(__name__)
-app.config.from_object(ProductionConfig())
 db = SQLAlchemy(app)
 
 if __name__ == "__main__":
