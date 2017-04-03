37bff80327b53f7ea4ac0c0f95127302b5791eed
lederp23 <munalaoliverngoitsi@gmail.com>
2017-04-03 10:31:45 +0300

    Modified manage.py

--- manage.py
+++ manage.py
@@ -10,29 +10,29 @@ from config import ProductionConfig
 from urls import urls
 from v1.api.models import BucketList, Item
 from v1.accounts.models import User
-from app import app, db
+import app
 
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app.app, resources={r"/api/*": {"origins": "*"}})
 
-migrate = Migrate(app, db)
-app.config.from_object(ProductionConfig())
-app.register_blueprint(urls)
-app.url_map.strict_slashes = False
-manager = Manager(app)
+migrate = Migrate(app.app, app.db)
+app.app.config.from_object(ProductionConfig())
+app.app.register_blueprint(urls)
+app.app.url_map.strict_slashes = False
+manager = Manager(app.app)
 manager.add_command('db', MigrateCommand)
 
 
 @manager.command
 def create_db():
     """Creates database with tables"""
-    db.create_all()
-    db.session.commit()
+    app.db.create_all()
+    app.db.session.commit()
 
 
 @manager.command
 def drop_db():
     """Creates database with tables"""
-    db.drop_all()
+    app.db.drop_all()
 
 if __name__ == "__app__":
     manager.run()
